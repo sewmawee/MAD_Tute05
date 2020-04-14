0e56e463dd8361ed89187eddfb6c097b793d2763
Sewwandi Maweekumbura <49789125+sewmawee@users.noreply.github.com>
2020-04-14 22:19:00 +0530

    Tute 05

---
+++ app/src/main/java/com/example/testapplicationfirebase/Student.java
@@ -0,0 +1,45 @@
+package com.example.testapplicationfirebase;
+
+public class Student {
+
+    private String ID;
+    private String name;
+    private String address;
+    private Integer contact;
+
+    public Student() {
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getContact() {
+        return contact;
+    }
+
+    public void setContact(Integer contact) {
+        this.contact = contact;
+    }
+}
+
